860fc5a31934438118d6bb01506322eab19bb78e
chantore <wnrnrxla@gmail.com>
2022-06-06 01:10:19 +0900

    New : Week3 File
    PPT, PDF, R

---
+++ Week3/Week3-기초통계(3).R
@@ -0,0 +1,18 @@
+#변수 설정
+Num <- 10
+Logi <- TRUE
+Char <- "Soongsil"
+Fac <- c("Korea", "Japan", "USA", "China", "England", "Germany")
+Fac <- as.factor(Fac)
+
+#class 함수
+class(Num)
+class(Logi)
+class(Char)
+class(Fac)
+
+#str 함수
+str(Num)
+str(Logi)
+str(Char)
+str(Fac)
